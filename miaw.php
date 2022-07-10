e13d7ad90b00a53446038c9728d9c8b0aa126ca0
Kocheng1337 <83726252+Kocheng1337@users.noreply.github.com>
2022-07-11 01:33:02 +0700

    Create miaw.php

---
+++ miaw.php
@@ -0,0 +1,54 @@
+
+<html>
+  <meta name="description" content="SABUNMANDI CYBER TEAM">
+  <meta name="author" content="And404_">
+  <META NAME="keywords" CONTENT="Hacked By And404_">
+  <META NAME="robots" CONTENT="index,follow">
+  <META NAME="revisit-after" CONTENT="3">
+  <meta name="theme-color" content="red">
+  <title>#Hacked By And404_</title> 
+  <meta property="og:image" content="https://k.top4top.io/p_203490j4x1.jpg">
+  <link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet" type="text/css"> 
+  <style>
+ body {
+        color: #FFFFFF;
+        font-family: 'Kelly Slab', sans-serif; 
+        font-weight: 60; 
+        height: 80vh;
+        margin: 0; 
+        background-color: black;
+}
+.full-height {
+        height: 67vh; 
+}
+.flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center; 
+}
+.position-ref {
+        position: relative; 
+}
+.content {
+        text-align: center; 
+}
+</style>
+</head>
+ <body>
+  <div class="stars"> 
+   <div class="twinkling"> 
+    <div class="flex-center position-ref full-height"> 
+     <div class="content"> 
+      <div class="text"> 
+       <img src="https://i.giphy.com/media/QmH8OnsBQvC4yn8BnX/giphy.webp" height="300"><br>
+ <i><font color="white" size="6">#Hacked by <font color="#42f542">And404_<br></font></font></i><font color="white" size="6"><font color="#2BFF05">
+        <font color="red" size="5">SABUNMANDI CYBER TEAM</font><br>
+        <font color="white" size="3">izm.sagiri404@gmail.com</font>
+     <audio src="https://jogosuroboyo2407.com/sby/pages/foto_bansos/a.mp3" loop="1" autoplay="1"></audio>
+     </font>
+    </font>
+   <i>
+ <br>
+<br>
+</body>
+</font></font></body></html>
